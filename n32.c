063392925b38a90410ec6fb3ef6e169404158e97
Teixeira008 <166056748+Teixeira008@users.noreply.github.com>
2025-05-16 17:09:56 -0300

    Update n32.c

--- n32.c
+++ n32.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 
 int main() {
-    int A, B, C, D, E, APROVADO, REPROVADO;
-    float nota1, nota2
+    char aproveitamento;
+    float nota1, nota2, media;
     printf("digite uma nota do seu boletim");
     scanf("%f", &nota1);
     printf("digite outra nota do seu boletim");
     scanf("%f", &nota2);
     
-    nota1 = 
-    if(nota >= 9.0 && nota <= 10.0 ){
-        printf("Aprovado\n");
+    media = (nota1 + nota2) / 2;
+    if(media >= 9.0 && media <= 10.0 ){
+        aproveitamento = 'A';
+    } else if ( media >= 7.5 && media < 9.0){
+        aproveitamento = 'B';
+    } else if (media >= 6.0 && media < 7.5){
+         aproveitamento = 'C';
+        } else if( media >= 4.0 && media < 6.0){
+            aproveitamento = 'D';
+        } else if (media >= 0.0 && media < 4.0){
+            aproveitamento = 'E';
+        }
+       if (aproveitamento == 'D' || aproveitamento == 'E'){
+        printf("REPROVADO\nAproveitamento: %c", aproveitamento);
+       }else{
+        printf("APROVADO\nAproveitamento: %c", aproveitamento);
+    return 0;
     }
+
 }
